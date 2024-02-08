856f6707b618a3f847ab5b4095cc109b650d8f2d
hitenwebs <hiteng@websoptimization>
2024-02-08 18:36:27 +0530

    Init

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'Webs',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://www.websoptimization.com/', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'https://www.websoptimization.com/', 
+    github: 'https://www.websoptimization.com/',
+    twitter: 'https://www.websoptimization.com/',
+    facebook: 'https://www.websoptimization.com/',
+    youtube: 'https://www.websoptimization.com/',
+    linkedin: 'https://www.websoptimization.com/',
+    dribbble: 'https://www.websoptimization.com/',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
