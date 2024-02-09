b67834c5b49495d4740658cd7078a339e5b771fc
hitenwebs <hiteng@websoptimization>
2024-02-09 10:33:31 +0530

    Next JS Migration

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const {withContentlayer} = require("next-contentlayer")
 const nextConfig = {
     compiler:{
         removeConsole: true,
-    }
+    },
+    reactStrictMode: true
 };
 
 module.exports = withContentlayer({ ...nextConfig });
